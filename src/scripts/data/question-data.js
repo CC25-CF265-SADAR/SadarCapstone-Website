16797619ea63548bf60fdf5e298300f702b1a8c9
Puti Narita Adzra <studentubacid@student.ac.id>
2025-05-17 23:15:41 +0700

    question data dummy

---
+++ src/scripts/data/question-data.js
@@ -0,0 +1,92 @@
+export const questions = [
+  {
+    id: 1,
+    type: "mcq",
+    question: "Apa ibu kota Indonesia?",
+    options: ["Bandung", "Jakarta", "Surabaya", "Bali"],
+    answer: "Jakarta"
+  },
+  {
+    id: 2,
+    type: "dragdrop",
+    question: "Pasangkan negara dengan ibukotanya",
+    pairs: [
+      { left: "Indonesia", right: "Jakarta" },
+      { left: "Malaysia", right: "Kuala Lumpur" },
+    ]
+  },
+  {
+    id: 3,
+    type: "mcq",
+    question: "Apa ibu kota Indonesia?",
+    options: ["Bandung", "Jakarta", "Surabaya", "Bali"],
+    answer: "Jakarta"
+  },
+  {
+    id: 4,
+    type: "mcq",
+    question: "Apa ibu kota Indonesia?",
+    options: ["Bandung", "Jakarta", "Surabaya", "Bali"],
+    answer: "Jakarta"
+  },
+  {
+    id: 5,
+    type: "mcq",
+    question: "Apa ibu kota Indonesia?",
+    options: ["Bandung", "Jakarta", "Surabaya", "Bali"],
+    answer: "Jakarta"
+  },
+  {
+    id: 6,
+    type: "dragdrop",
+    question: "Pasangkan negara dengan ibukotanya",
+    pairs: [
+      { left: "Indonesia", right: "Jakarta" },
+      { left: "Malaysia", right: "Kuala Lumpur" },
+    ]
+  },
+  {
+    id: 7,
+    type: "mcq",
+    question: "Apa ibu kota Indonesia?",
+    options: ["Bandung", "Jakarta", "Surabaya", "Bali"],
+    answer: "Jakarta"
+  },
+  {
+    id: 8,
+    type: "mcq",
+    question: "Apa ibu kota Indonesia?",
+    options: ["Bandung", "Jakarta", "Surabaya", "Bali"],
+    answer: "Jakarta"
+  },
+  {
+    id: 9,
+    type: "mcq",
+    question: "Apa ibu kota Indonesia?",
+    options: ["Bandung", "Jakarta", "Surabaya", "Bali"],
+    answer: "Jakarta"
+  },
+  {
+    id: 10,
+    type: "dragdrop",
+    question: "Pasangkan negara dengan ibukotanya",
+    pairs: [
+      { left: "Indonesia", right: "Jakarta" },
+      { left: "Malaysia", right: "Kuala Lumpur" },
+    ]
+  },
+  {
+    id: 11,
+    type: "mcq",
+    question: "Apa ibu kota Indonesia?",
+    options: ["Bandung", "Jakarta", "Surabaya", "Bali"],
+    answer: "Jakarta"
+  },
+  {
+    id: 12,
+    type: "mcq",
+    question: "Apa ibu kota Indonesia?",
+    options: ["Bandung", "Jakarta", "Surabaya", "Bali"],
+    answer: "Jakarta"
+  },
+];
