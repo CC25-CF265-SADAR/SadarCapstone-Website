24ce6927caafee7a7c1601e3e44131687977c568
noraaxzsy <alyahudanis9@gmail.com>
2025-06-09 20:59:47 +0700

    perbaikan landingpage-info responsif

--- src/scripts/pages/homepage/homepage-page.js
+++ src/scripts/pages/homepage/homepage-page.js
@@ -17,6 +17,7 @@ export default class Homepage {
 
   async render() {
     return `
+      <div class="max-w-screen-xl ">
             <section class="hero-image">
                 <section class="bg-white sm:grid sm:grid-cols-2">
                   <div class="p-8 md:p-12 lg:px-16 lg:py-24">
@@ -51,20 +52,20 @@ export default class Homepage {
             </section>
             </section>
 
-            <section class="info-1 font-jakarta px-4 sm:px-6 lg:px-8 py-8 max-w-screen-xl mx-auto">
-                <h1 class="font-bold text-3xl sm:text-4xl text-center mb-10">
+            <section class="info-1 font-jakarta px-4 sm:px-6 lg:px-8 py-8 max-w-screen-xl my-12">
+                <h1 class="font-semibold text-2xl sm:text-3xl text-center mb-3 md:mb-10 py-8">
                     <span class="text-[#42A7C3]">Pertumbuhan Penetrasi Internet</span> di Indonesia
                 </h1>
 
-                <article class="article-info-1 flex flex-col lg:flex-row lg:gap-10 items-center lg:items-start justify-between">
+                <article class="article-info-1 flex flex-col lg:flex-row lg:gap-10 items-center lg:items-start justify-between px-8">
                     <section class="flex flex-col gap-4 max-w-xl mx-auto lg:mx-0 text-center lg:text-left">
-                        <h2 class="text-2xl sm:text-3xl font-semibold">Tahukah Kamu?</h2>
-                        <p class="text-lg sm:text-2xl"> Selama tujuh tahun terakhir, jumlah penduduk terkoneksi internet tahun 2024 mencapai
+                        <h2 class="text-xl sm:text-3xl font-semibold">Tahukah Kamu?</h2>
+                        <p class="text-lg sm:text-xl"> Selama tujuh tahun terakhir, jumlah penduduk terkoneksi internet tahun 2024 mencapai
                         <span class="font-bold inline-block text-white bg-[#378BA2] px-1"> 221.563.479 </span> jiwa dari total populasi 278.696.200 jiwa penduduk Indonesia.</p>
                         <p class="text-sm sm:text-base"> Sumber: Asosiasi Penyelenggara Jasa Internet Indonesia (APJII) 2024</p>
                     </section>
 
-                    <aside class="w-full max-w-xs sm:max-w-md mt-8 lg:mt-0">
+                    <aside class="w-full max-w-xs sm:max-w-sm mt-8 lg:mt-0">
                         <img
                         src="images/grafik-data-penetrasi.svg"
                         alt="Grafik penetrasi di Indonesia"
@@ -74,22 +75,22 @@ export default class Homepage {
                  </article>
             </section>
 
-            <section class="info-2 flex flex-col lg:flex-row items-center lg:items-start justify-center gap-8 lg:gap-12 max-w-screen-xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-8">
-                <article class="flex flex-col gap-6 w-full lg:w-3/5 text-center lg:text-left">
-                    <h1 class="font-bold text-3xl sm:text-4xl leading-snug">
+            <section class="info-2 flex flex-col lg:flex-row lg:gap-10 items-center lg:items-start justify-between px-8 my-12">
+                <article class="flex flex-col gap-4 max-w-xl mx-auto lg:mx-0 text-center lg:text-left px-8">
+                    <h1 class="font-semibold text-2xl sm:text-3xl">
                     <span class="text-[#42A7C3]">Korban</span> Penipuan Online
                     </h1>
 
-                    <p class="text-base sm:text-lg">
+                    <p class="text-md sm:text-xl">
                     <span class="font-bold">Rendahnya tingkat literasi digital</span> masyarakat Indonesia menyebabkan masih banyak kelompok umur yang rentan menjadi korban penipuan online.
                     </p>
 
-                    <p class="text-base sm:text-lg">
-                    Meskipun tingkat penetrasi/pengguna internet semakin tinggi, sebagian besar masyarakat belum memiliki kebiasaan atau kemampuan memadai dalam memverifikasi informasi, menjaga keamanan data pribadi, atau mengenali bentuk‑bentuk penipuan digital yang makin canggih.
+                    <p class="text-md sm:text-xl">
+                    Meskipun pengguna internet semakin tinggi, sebagian besar masyarakat belum memiliki kebiasaan atau kemampuan memadai dalam memverifikasi informasi, menjaga keamanan data pribadi, atau mengenali bentuk‑bentuk penipuan digital yang makin canggih.
                     </p>
                 </article>
 
-                <aside class="w-full max-w-sm sm:max-w-md lg:max-w-[550px] lg:h-[415px] flex-shrink-0 mt-8 lg:mt-0">
+                <aside class="w-full max-w-xs sm:max-w-md mt-8 lg:mt-0">
                     <img
                     src="images/korban-penipuan-online.png"
                     alt="Korban penipuan online di Indonesia"
@@ -98,79 +99,78 @@ export default class Homepage {
                 </aside>
             </section>
 
-            <section class="sadar-feature relative flex flex-col gap-8 min-h-[600px]">
+            <section class="sadar-feature relative flex flex-col gap-8 min-h-[200px] my-10">
                 <img
-                    src="images/background-sadar.png"
+                    src="images/background_type.svg"
                     alt="Background sadar"
                     class="absolute inset-0 -z-10 w-full h-full object-cover"
                 />
 
                 <article class="text-white flex flex-col gap-4 z-10 mt-10 px-4 text-center">
                     <h1 class="text-2xl sm:text-3xl font-bold">SADAR Mulai Dari Sekarang!</h1>
-                    <p class="text-base sm:text-lg max-w-xl mx-auto">
+                    <p class="text-md md:text-lg max-w-xl md:max-w-2xl mx-auto">
                     Gunakan layanan kami untuk meningkatkan pengalaman Anda dalam menjaga informasi dan data dari penipuan online
                     </p>
                 </article>
 
-                <section class="sadar-feature-detail z-10 max-w-7xl mx-auto w-full px-4 sm:px-6 lg:px-8 grid grid-cols-1 lg:grid-cols-[1fr_1fr] lg:grid-rows-2 gap-6 pb-12">
-                    <div class="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg shadow-sm p-6 flex flex-col gap-5 lg:row-span-2">
-                        <img src="images/ikon-cekajadulu.svg" class="w-[76px] h-[76px]" alt="Ikon CekAjaDulu" />
-                        <h2 class="text-2xl font-semibold text-gray-900 dark:text-white">CekAjaDulu</h2>
-                        <p class="text-gray-500 dark:text-gray-400">
-                            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam nec tortor massa. Praesent cursus porttitor egestas.
-                        </p>
-                        <div class="flex flex-row gap-6">
-                            <a href="#/link-checking/cek-umum" onclick="window.scrollTo(0, 0)"  class="inline-flex font-medium items-center text-[#42A7C3] hover:underline">
-                                Lihat Fitur
-                                <svg class="w-3.5 h-3.5 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                </svg>
-                            </a>
-                        </div>
+                <section class="sadar-feature-detail z-10 max-w-7xl mx-auto w-full px-4 sm:px-6 lg:px-8 grid grid-cols-1 lg:grid-cols-3 gap-6 pb-12">
+                    <!-- CekAjaDulu -->
+                    <div class="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg shadow-sm p-6 flex flex-col gap-5">
+                      <img src="images/ikon-cekajadulu.svg" class="w-[72px] h-[72px]" alt="Ikon CekAjaDulu" />
+                      <h2 class="text-lg md:text-xl font-semibold text-gray-900 dark:text-white">CekAjaDulu</h2>
+                      <p class="text-gray-500 dark:text-gray-400">
+                        Sistem cerdas berbasis machine learning dan NLP untuk mendeteksi pesan spam dan tautan phising.
+                      </p>
+                      <div class="flex flex-row gap-6">
+                        <a href="#/link-checking/cek-umum" onclick="window.scrollTo(0, 0)" class="inline-flex font-medium items-center text-[#42A7C3] hover:underline">
+                          Lihat Fitur
+                          <svg class="w-3.5 h-3.5 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M1 5h12m0 0L9 1m4 4L9 9" />
+                          </svg>
+                        </a>
+                      </div>
                     </div>
 
-                    <div class="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg shadow-sm p-6 flex gap-5 items-start">
-                        <img src="images/ikon-tipumeter.svg" class="w-[76px] h-[76px]" alt="Ikon TipuMeter" />
-                        <div>
-                            <h2 class="text-xl font-semibold text-gray-900 dark:text-white mb-2">TipuMeter</h2>
-                            <p class="text-gray-500 dark:text-gray-400 mb-3">
-                            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
-                            </p>
-                            <a href="#/module-overview" onclick="window.scrollTo(0, 0)" class="inline-flex font-medium items-center text-[#42A7C3] hover:underline">
-                            Lihat Fitur
-                            <svg class="w-3.5 h-3.5 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M1 5h12m0 0L9 1m4 4L9 9" />
-                            </svg>
-                            </a>
-                        </div>
+                    <!-- TipuMeter -->
+                    <div class="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg shadow-sm p-6 flex flex-col gap-5">
+                      <img src="images/ikon-tipumeter.svg" class="w-[72px] h-[72px]" alt="Ikon TipuMeter" />
+                      <h2 class="text-lg md:text-xl font-semibold text-gray-900 dark:text-white">TipuMeter</h2>
+                      <p class="text-gray-500 dark:text-gray-400">
+                        Fitur gamifikasi berupa simulasi nyata yang mengukur kewaspadaan pengguna terhadap penipuan digital.
+                      </p>
+                      <a href="#/quiz" onclick="window.scrollTo(0, 0)" class="inline-flex font-medium items-center text-[#42A7C3] hover:underline">
+                        Lihat Fitur
+                        <svg class="w-3.5 h-3.5 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                      </a>
                     </div>
 
-                    <div class="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg shadow-sm p-6 flex gap-5 items-start">
-                        <img src="images/ikon-antitertipu.svg" class="w-[76px] h-[76px]" alt="Ikon AntiTertipu" />
-                        <div>
-                            <h2 class="text-xl font-semibold text-gray-900 dark:text-white mb-2">AntiTertipu</h2>
-                            <p class="text-gray-500 dark:text-gray-400 mb-3">
-                            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
-                            </p>
-                            <a href="#/quiz" onclick="window.scrollTo(0, 0)" class="inline-flex font-medium items-center text-[#42A7C3] hover:underline">
-                            Lihat Fitur
-                            <svg class="w-3.5 h-3.5 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M1 5h12m0 0L9 1m4 4L9 9" />
-                            </svg>
-                            </a>
-                        </div>
+                    <!-- AntiTertipu -->
+                    <div class="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg shadow-sm p-6 flex flex-col gap-5">
+                      <img src="images/ikon-antitertipu.svg" class="w-[72px] h-[72px]" alt="Ikon Antitertipu" />
+                      <h2 class="text-lg md:text-xl font-semibold text-gray-900 dark:text-white">Antitertipu</h2>
+                      <p class="text-gray-500 dark:text-gray-400">
+                        Platform interaktif yang mengedukasi pengguna tentang penipuan digital melalui materi multimedia dan kuis.
+                      </p>
+                      <a href="#/module-overview" onclick="window.scrollTo(0, 0)" class="inline-flex font-medium items-center text-[#42A7C3] hover:underline">
+                        Lihat Fitur
+                        <svg class="w-3.5 h-3.5 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                      </a>
                     </div>
-                </section>
+                  </section>
             </section>
 
-            <section class="leaderboard">
+            <section class="my-10">
                 ${generateLeaderboardLinkTemplate()}
             </section>
             
-            <section class="faq bg-white font-jakarta flex items-center py-12 px-18">
+            <section class="faq bg-white font-jakarta flex items-center py-12 px-18 my-10">
                 <div class="w-full">
                     <h2 class="text-4xl font-semibold text-center text-black mb-2">Pertanyaan yang Sering Diajukan</h2>
                     <p class="text-base text-[#6D7280] text-center max-w-xl mx-auto mb-6">
@@ -192,6 +192,8 @@ export default class Homepage {
             </section>
 
             ${generateFooterTemplate()}
+      </div>
+            
         `;
   }
 
@@ -205,15 +207,15 @@ export default class Homepage {
         location.reload(); // agar render ulang navbar
       }
     });
-    
+
     this.presenter.setFAQData(faqList);
     this.presenter.renderFAQ('all');
 
-    document.querySelectorAll('.faq-tab').forEach(btn => {
+    document.querySelectorAll('.faq-tab').forEach((btn) => {
       btn.addEventListener('click', () => {
         const category = btn.dataset.category;
 
-        document.querySelectorAll('.faq-tab').forEach(tab => {
+        document.querySelectorAll('.faq-tab').forEach((tab) => {
           tab.classList.remove('bg-[#42A7C3]', 'text-white');
           tab.classList.add('bg-white', 'text-gray-600');
         });
@@ -231,7 +233,9 @@ export default class Homepage {
 
       try {
         const data = await fetchPhishingLeaderboard(monthOnly);
-        listEl.innerHTML = data.map((item, idx) => `
+        listEl.innerHTML = data
+          .map(
+            (item, idx) => `
           <li class="flex items-start gap-3 p-4 border rounded-xl bg-white shadow-sm">
             <div class="w-12 h-12 rounded-lg bg-[#FFF1AA] text-[#2C6F82] text-xl font-bold flex items-center justify-center">${idx + 1}</div>
             <div>
@@ -239,7 +243,9 @@ export default class Homepage {
               <p class="text-base font-regular text-gray-500">telah dicari sebanyak ${item.count} kali</p>
             </div>
           </li>
-        `).join('');
+        `,
+          )
+          .join('');
       } catch (err) {
         listEl.innerHTML = `<li class="text-red-500">Gagal memuat leaderboard: ${err.message}</li>`;
       }
@@ -265,7 +271,7 @@ export default class Homepage {
     btnMonth?.addEventListener('click', () => {
       btnMonth.classList.add('bg-[#42A7C3]', 'text-white');
       btnMonth.classList.remove('bg-white', 'text-gray-600');
-    
+
       btnAll.classList.remove('bg-[#42A7C3]', 'text-white');
       btnAll.classList.add('bg-white', 'text-gray-600');
 
@@ -279,7 +285,9 @@ export default class Homepage {
 
   renderFAQList(filteredFaq) {
     const container = document.getElementById('faq-content');
-    container.innerHTML = filteredFaq.map(faq => `
+    container.innerHTML = filteredFaq
+      .map(
+        (faq) => `
       <details class="group border border-gray-200 rounded-2xl p-6 bg-white open:bg-[#DFF0F5] transition-colors duration-100">
         <summary class="flex items-center justify-between cursor-pointer">
           <h3 class="text-xl font-medium text-gray-900">${faq.question}</h3>
@@ -292,6 +300,8 @@ export default class Homepage {
         </summary>
         <p class="mt-4 text-gray-700 text-base leading-relaxed">${faq.answer}</p>
       </details>
-    `).join('');
+    `,
+      )
+      .join('');
   }
 }
