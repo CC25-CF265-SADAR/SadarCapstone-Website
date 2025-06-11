dd628000869928976c14653c336c3550dec79d26
noraaxzsy <alyahudanis9@gmail.com>
2025-06-11 11:24:23 +0700

    deploy backend

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  BASE_URL: 'http://localhost:5000',
+  BASE_URL: 'https://api-sadar.onrender.com',
   ACCESS_TOKEN_KEY: 'sadar_access_token',
   GOOGLE_CLIENT_ID: '323941138654-aid5diul9df0c2n5u6pokervn0a75gcm.apps.googleusercontent.com',
   BASE_URL_SPAM: 'https://spam-detection-sadar.up.railway.app',
